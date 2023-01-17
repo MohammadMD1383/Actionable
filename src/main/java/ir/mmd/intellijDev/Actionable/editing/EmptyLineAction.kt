6352869dc481fe33ea68a830ed4faf5fd5734236
Mohammad Mostafa <ezmmd83@gmail.com>
2023-01-17 14:04:13 +0330

    implemented EmptyLineAction, fixes #15

---
+++ src/main/java/ir/mmd/intellijDev/Actionable/editing/EmptyLineAction.kt
@@ -0,0 +1,27 @@
+package ir.mmd.intellijDev.Actionable.editing
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import ir.mmd.intellijDev.Actionable.internal.proguard.Keep
+import ir.mmd.intellijDev.Actionable.util.ext.*
+
+@Keep
+class EmptyLineAction : AnAction() {
+	override fun actionPerformed(e: AnActionEvent) {
+		val editor = e.editor
+		val document = editor.document
+		
+		e.allCarets.forEach {
+			val lineNumber = document.getLineNumber(it.offset)
+			val lineStartOffset = document.getLineStartOffset(lineNumber)
+			val lineEndOffset = document.getLineEndOffset(lineNumber)
+			
+			e.project.runWriteCommandAction {
+				document.deleteString(lineStartOffset, lineEndOffset)
+			}
+		}
+	}
+	
+	override fun isDumbAware() = true
+	override fun update(e: AnActionEvent) = e.enableIf { hasProject and hasEditor }
+}
