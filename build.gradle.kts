fb0e5d044aa8ec3c468bfcd9a23790eb0702643c
Mohammad Mostafa <ezmmd83@gmail.com>
2023-01-19 11:18:56 +0330

    updated build.gradle.kts and plugin.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ fun DependencyHandlerScope.classpath(path: String) {
 }
 
 group = "ir.mmd.intellijDev"
-version = "3.12.0"
+version = "3.13.0"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_1_8
