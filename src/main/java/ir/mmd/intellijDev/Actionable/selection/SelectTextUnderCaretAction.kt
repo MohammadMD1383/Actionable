c87b67e6d7a6634ed0adbffbdaa2ab9d9aeacf51
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 16:37:09 +0330

    #21: Base.kt -> SelectTextUnderCaretAction

--- src/main/java/ir/mmd/intellijDev/Actionable/selection/SelectTextUnderCaretAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/selection/SelectTextUnderCaretAction.kt
@@ -1,17 +1,21 @@
 package ir.mmd.intellijDev.Actionable.selection
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.editor.Caret
 import com.intellij.psi.PsiFile
-import ir.mmd.intellijDev.Actionable.util.ext.*
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretAction
+import ir.mmd.intellijDev.Actionable.util.ext.enableIf
+import ir.mmd.intellijDev.Actionable.util.ext.hasEditor
+import ir.mmd.intellijDev.Actionable.util.ext.hasProject
 
-abstract class SelectTextUnderCaretAction : AnAction() {
+abstract class SelectTextUnderCaretAction : MultiCaretAction() {
 	abstract fun getSelectionRange(caret: Caret, psiFile: PsiFile): IntRange?
 	
-	override fun actionPerformed(e: AnActionEvent) = e.editor.allCarets.forEach { caret ->
-		val range = getSelectionRange(caret, e.psiFile) ?: return@forEach
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		val range = getSelectionRange(caret, psiFile) ?: return
 		caret.setSelection(range.first, range.last)
 	}
 	
