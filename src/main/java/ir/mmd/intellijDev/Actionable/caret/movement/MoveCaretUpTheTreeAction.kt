f1062d4741e7edccb7fe80524407864c49421aea
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 15:24:34 +0330

    #21: MoveCaretUpTheTreeAction

--- src/main/java/ir/mmd/intellijDev/Actionable/caret/movement/MoveCaretUpTheTreeAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/caret/movement/MoveCaretUpTheTreeAction.kt
@@ -1,22 +1,21 @@
 package ir.mmd.intellijDev.Actionable.caret.movement
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.editor.Caret
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretAction
 import ir.mmd.intellijDev.Actionable.util.ext.*
 
-class MoveCaretUpTheTreeAction : AnAction() {
-	override fun actionPerformed(e: AnActionEvent) {
-		val psiFile = e.psiFile
-		
-		e.allCarets.forEach { caret ->
-			var element = psiFile.elementAt(caret)?.parentNoWhitespace ?: return@forEach
-			if (element.textRange.startOffset == caret.offset) {
-				element = element.parentNoWhitespace ?: return@forEach
-			}
-			
-			caret moveTo element.textRange.startOffset
+class MoveCaretUpTheTreeAction : MultiCaretAction() {
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		var element = psiFile.elementAt(caret)?.parentNoWhitespace ?: return
+		if (element.textRange.startOffset == caret.offset) {
+			element = element.parentNoWhitespace ?: return
 		}
+		
+		caret moveTo element.textRange.startOffset
 	}
 	
 	override fun update(e: AnActionEvent) = e.enableIf { hasProject and hasEditor }
