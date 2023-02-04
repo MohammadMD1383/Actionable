01fae20ad82b284afedf605f91e31e36aa50d161
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 16:24:21 +0330

    #21: Base.kt -> DeleteToIndentedLineStartAction and fixes #18

--- src/main/java/ir/mmd/intellijDev/Actionable/editing/DeleteToIndentedLineStartAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/editing/DeleteToIndentedLineStartAction.kt
@@ -1,29 +1,34 @@
 package ir.mmd.intellijDev.Actionable.editing
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-
+import com.intellij.openapi.editor.Caret
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretAction
 import ir.mmd.intellijDev.Actionable.util.ext.*
 
 
-class DeleteToIndentedLineStartAction : AnAction() {
-	override fun actionPerformed(e: AnActionEvent) {
-		val editor = e.editor
-		val document = editor.document
+class DeleteToIndentedLineStartAction : MultiCaretAction() {
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		val lineNumber = document.getLineNumber(caret.offset)
+		val lineStartOffset = document.getLineStartOffset(lineNumber)
+		val lineEndOffset = document.getLineEndOffset(lineNumber)
+		val lineIndentEndOffset = lineStartOffset + document.getLineStartIndentLength(lineNumber)
+		val line = document.getText(lineStartOffset..lineEndOffset)
 		
-		e.project.runWriteCommandAction {
-			e.allCarets.forEach {
-				val lineNumber = document.getLineNumber(it.offset)
-				val lineStartOffset = document.getLineStartOffset(lineNumber)
-				val lineEndOffset = document.getLineEndOffset(lineNumber)
-				val lineIndentEndOffset = lineStartOffset + document.getLineStartIndentLength(lineNumber)
-				val line = document.getText(lineStartOffset..lineEndOffset)
-				
-				if (line.isBlank()) {
+		project.runWriteCommandAction {
+			when {
+				line.isBlank() -> {
 					document.deleteString(lineStartOffset, lineEndOffset)
-				} else if (it.offset > lineIndentEndOffset) {
-					document.deleteString(lineIndentEndOffset, it.offset)
+				}
+				
+				caret.offset <= lineIndentEndOffset -> {
+					document.deleteString(lineStartOffset, caret.offset)
+				}
+				
+				caret.offset > lineIndentEndOffset -> {
+					document.deleteString(lineIndentEndOffset, caret.offset)
 				}
 			}
 		}
