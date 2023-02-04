a6290b542de5258005c5e4c8613e9f9ea819d096
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 16:26:57 +0330

    #21: Base.kt -> IndentPreservingEmptyLineAction

--- src/main/java/ir/mmd/intellijDev/Actionable/editing/IndentPreservingEmptyLineAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/editing/IndentPreservingEmptyLineAction.kt
@@ -1,31 +1,28 @@
 package ir.mmd.intellijDev.Actionable.editing
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-
+import com.intellij.openapi.editor.Caret
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretAction
 import ir.mmd.intellijDev.Actionable.util.ext.*
 
 
-class IndentPreservingEmptyLineAction : AnAction() {
-	override fun actionPerformed(e: AnActionEvent) {
-		val editor = e.editor
-		val document = editor.document
+class IndentPreservingEmptyLineAction : MultiCaretAction() {
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		val lineNumber = document.getLineNumber(caret.offset)
+		val lineStartOffset = document.getLineStartOffset(lineNumber)
+		val lineEndOffset = document.getLineEndOffset(lineNumber)
+		val lineIndentEndOffset = lineStartOffset + document.getLineStartIndentLength(lineNumber)
+		val line = document.getText(lineStartOffset..lineEndOffset)
 		
-		e.project.runWriteCommandAction {
-			e.allCarets.forEach {
-				val lineNumber = document.getLineNumber(it.offset)
-				val lineStartOffset = document.getLineStartOffset(lineNumber)
-				val lineEndOffset = document.getLineEndOffset(lineNumber)
-				val lineIndentEndOffset = lineStartOffset + document.getLineStartIndentLength(lineNumber)
-				val line = document.getText(lineStartOffset..lineEndOffset)
-				
-				if (line.isBlank()) {
-					document.deleteString(lineStartOffset, lineEndOffset)
-				} else {
-					it moveTo lineIndentEndOffset
-					document.deleteString(lineIndentEndOffset, lineEndOffset)
-				}
+		project.runWriteCommandAction {
+			if (line.isBlank()) {
+				document.deleteString(lineStartOffset, lineEndOffset)
+			} else {
+				caret moveTo lineIndentEndOffset
+				document.deleteString(lineIndentEndOffset, lineEndOffset)
 			}
 		}
 	}
