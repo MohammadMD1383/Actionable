04777998aca1cae284ca59b15eee10aacccc345d
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 14:17:48 +0330

    #21: ActionHelper.kt, Actions.java

--- src/main/java/ir/mmd/intellijDev/Actionable/action/ActionHelper.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/action/ActionHelper.kt
@@ -5,16 +5,31 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import ir.mmd.intellijDev.Actionable.text.macro.MacroAction
 
+/**
+ * Returns `MacrosActionGroup` as a [DefaultActionGroup] to be used for adding custom macros
+ *
+ * @see ir.mmd.intellijDev.Actionable.text.macro.AddSelectionToMacrosAction
+ */
 val MacrosActionGroup by lazy {
 	ActionManager.getInstance().getAction(Actions.Groups.MACROS) as DefaultActionGroup
 }
 
+/**
+ * Registers a macro
+ *
+ * @see ir.mmd.intellijDev.Actionable.text.macro.AddSelectionToMacrosAction
+ */
 fun ActionManager.registerMacro(name: String, macro: String) {
 	val action = MacroAction(name, macro)
 	registerAction("${Actions.MACRO_PREFIX}.$name", action)
 	MacrosActionGroup.add(action)
 }
 
+/**
+ * Unregisters a macro
+ *
+ * @see ir.mmd.intellijDev.Actionable.text.macro.AddSelectionToMacrosAction
+ */
 fun ActionManager.unregisterMacro(name: String) {
 	val actionId = "${Actions.MACRO_PREFIX}.$name"
 	val action = getAction(actionId)!!
@@ -23,4 +38,8 @@ fun ActionManager.unregisterMacro(name: String) {
 	MacrosActionGroup.remove(action)
 }
 
+/**
+ * Retrieves [AnAction] from its id
+ */
+@Suppress("NOTHING_TO_INLINE")
 inline fun action(id: String): AnAction? = ActionManager.getInstance().getAction(id)
--- src/main/java/ir/mmd/intellijDev/Actionable/action/Actions.java
+++ src/main/java/ir/mmd/intellijDev/Actionable/action/Actions.java
@@ -1,9 +1,21 @@
 package ir.mmd.intellijDev.Actionable.action;
 
+/**
+ * List of action names needed by this plugin
+ */
 public final class Actions {
+	private Actions() {
+	}
+	
 	public static final String MACRO_PREFIX = "Actionable.Macro";
 	
+	/**
+	 * List of action group names needed bt this plugin
+	 */
 	public static class Groups {
+		private Groups() {
+		}
+		
 		public static final String MACROS = "ir.mmd.intellijDev.Actionable.text.macro.MacrosActionGroup";
 	}
 }
