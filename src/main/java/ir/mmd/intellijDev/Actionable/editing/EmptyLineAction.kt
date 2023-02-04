50924b67cf6c45abe55a4fd54e551eb0e8b46cf8
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 16:25:35 +0330

    #21: Base.kt -> EmptyLineAction

--- src/main/java/ir/mmd/intellijDev/Actionable/editing/EmptyLineAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/editing/EmptyLineAction.kt
@@ -1,25 +1,25 @@
 package ir.mmd.intellijDev.Actionable.editing
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.editor.Caret
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretAction
+import ir.mmd.intellijDev.Actionable.util.ext.enableIf
+import ir.mmd.intellijDev.Actionable.util.ext.hasEditor
+import ir.mmd.intellijDev.Actionable.util.ext.hasProject
+import ir.mmd.intellijDev.Actionable.util.ext.runWriteCommandAction
 
-import ir.mmd.intellijDev.Actionable.util.ext.*
 
-
-class EmptyLineAction : AnAction() {
-	override fun actionPerformed(e: AnActionEvent) {
-		val editor = e.editor
-		val document = editor.document
+class EmptyLineAction : MultiCaretAction() {
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		val lineNumber = document.getLineNumber(caret.offset)
+		val lineStartOffset = document.getLineStartOffset(lineNumber)
+		val lineEndOffset = document.getLineEndOffset(lineNumber)
 		
-		e.project.runWriteCommandAction {
-			e.allCarets.forEach {
-				val lineNumber = document.getLineNumber(it.offset)
-				val lineStartOffset = document.getLineStartOffset(lineNumber)
-				val lineEndOffset = document.getLineEndOffset(lineNumber)
-				
-				document.deleteString(lineStartOffset, lineEndOffset)
-			}
+		project.runWriteCommandAction {
+			document.deleteString(lineStartOffset, lineEndOffset)
 		}
 	}
 	
