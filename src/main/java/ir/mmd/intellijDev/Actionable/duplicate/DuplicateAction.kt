9a1a304663bbcc6518d0d2c73fbe13e659113910
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 15:53:42 +0330

    #21: Base.kt -> DuplicateAction

--- src/main/java/ir/mmd/intellijDev/Actionable/duplicate/DuplicateAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/duplicate/DuplicateAction.kt
@@ -1,19 +1,23 @@
 package ir.mmd.intellijDev.Actionable.duplicate
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.editor.Caret
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretActionWithInitialization
 import ir.mmd.intellijDev.Actionable.util.DuplicateUtil
-import ir.mmd.intellijDev.Actionable.util.ext.*
+import ir.mmd.intellijDev.Actionable.util.ext.duplicator
+import ir.mmd.intellijDev.Actionable.util.ext.enableIf
+import ir.mmd.intellijDev.Actionable.util.ext.hasEditor
+import ir.mmd.intellijDev.Actionable.util.ext.hasProject
 
-abstract class DuplicateAction(private val duplicate: DuplicateUtil.(Int, Int) -> Unit) : AnAction() {
-	override fun actionPerformed(e: AnActionEvent) {
-		val editor = e.editor
-		val duplicator = editor.duplicator
-		
-		editor.caretModel.allCarets.withEach {
-			duplicator.duplicate(selectionStart, selectionEnd)
-		}
+abstract class DuplicateAction(private val duplicate: DuplicateUtil.(Int, Int) -> Unit) : MultiCaretActionWithInitialization<DuplicateUtil>() {
+	context (LazyEventContext)
+	override fun initialize() = editor.duplicator
+	
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		data.duplicate(caret.selectionStart, caret.selectionEnd)
 	}
 	
 	override fun isDumbAware() = true
