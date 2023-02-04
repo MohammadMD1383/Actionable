4e9689dca0e3c6c59ba89cce6f9f214475bc2542
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-04 16:05:29 +0330

    #21: Base.kt -> ExtractLineWithoutExtraWhitespaceAtCaret

--- src/main/java/ir/mmd/intellijDev/Actionable/caret/editing/ExtractLineWithoutExtraWhitespaceAtCaret.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/caret/editing/ExtractLineWithoutExtraWhitespaceAtCaret.kt
@@ -1,30 +1,34 @@
 package ir.mmd.intellijDev.Actionable.caret.editing
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.util.TextRange
 import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.action.MultiCaretActionWithInitialization
 import ir.mmd.intellijDev.Actionable.util.ext.*
 
-abstract class ExtractLineWithoutExtraWhitespaceAtCaret(private val cut: Boolean) : AnAction() {
-	override fun actionPerformed(e: AnActionEvent) = with(LazyEventContext(e)) {
-		val texts = mutableListOf<String>()
+abstract class ExtractLineWithoutExtraWhitespaceAtCaret(private val cut: Boolean) : MultiCaretActionWithInitialization<MutableList<String>>() {
+	context(LazyEventContext)
+	override fun initialize(): MutableList<String> = mutableListOf()
+	
+	context(LazyEventContext)
+	override fun finalize() {
+		data.joinToString(separator = "\n").copyToClipboard()
+	}
+	
+	context (LazyEventContext)
+	override fun perform(caret: Caret) {
+		val lineNumber = document.getLineNumber(caret.offset)
+		val start = document.getLineStartOffset(lineNumber) + document.getLineStartIndentLength(lineNumber)
+		val end = document.getLineEndOffset(lineNumber) - document.getLineTrailingWhitespaceLength(lineNumber)
 		
-		project.runWriteCommandAction {
-			allCarets.forEach {
-				val lineNumber = document.getLineNumber(it.offset)
-				val start = document.getLineStartOffset(lineNumber) + document.getLineStartIndentLength(lineNumber)
-				val end = document.getLineEndOffset(lineNumber) - document.getLineTrailingWhitespaceLength(lineNumber)
-				
-				texts += document.getText(TextRange(start, end))
-				if (cut) {
-					document.deleteString(start, end)
-				}
+		data += document.getText(TextRange(start, end))
+		if (cut) {
+			project.runWriteCommandAction {
+				document.deleteString(start, end)
 			}
 		}
-		
-		texts.joinToString(separator = "\n").copyToClipboard()
 	}
 	
 	override fun update(e: AnActionEvent) = e.enableIf { hasProject and hasEditor }
