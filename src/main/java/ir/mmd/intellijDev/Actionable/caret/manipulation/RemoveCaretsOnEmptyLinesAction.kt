0cd8772e2a31a32ed60cb7a432a0e590314d53d4
Jason <xfreelancerx@gmail.com>
2023-02-08 01:05:52 -0600

    Adding documentation

--- src/main/java/ir/mmd/intellijDev/Actionable/caret/manipulation/RemoveCaretsOnEmptyLinesAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/caret/manipulation/RemoveCaretsOnEmptyLinesAction.kt
@@ -6,8 +6,12 @@ import com.intellij.openapi.editor.Caret
 import ir.mmd.intellijDev.Actionable.action.LazyEventContext
 import ir.mmd.intellijDev.Actionable.action.MultiCaretAction
 import ir.mmd.intellijDev.Actionable.util.ext.*
+import ir.mmd.intellijDev.Actionable.internal.doc.Documentation
 
-
+@Documentation(
+	title = "Remove Carets On Empty Lines",
+	description = "Removes all carets that are on empty/blank lines.",
+)
 class RemoveCaretsOnEmptyLinesAction : MultiCaretAction() {
 	context (LazyEventContext)
 	override fun perform(caret: Caret) {
@@ -15,12 +19,12 @@ class RemoveCaretsOnEmptyLinesAction : MultiCaretAction() {
 		val startOffset = document.getLineStartOffset(lineNumber)
 		val endOffset = document.getLineEndOffset(lineNumber)
 		val line = document.getText(startOffset..endOffset)
-		
+
 		if (line.isBlank()) {
 			caretModel.removeCaret(caret)
 		}
 	}
-	
+
 	override fun update(e: AnActionEvent) = e.enableIf { hasProject and hasEditorWith { caretCount > 1 } }
 	override fun isDumbAware() = true
 	override fun getActionUpdateThread() = ActionUpdateThread.BGT
