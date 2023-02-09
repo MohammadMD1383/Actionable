fcd0505a8307c844049d36e8d059bff038a09f47
Mohammad Mostafa <ezmmd83@gmail.com>
2023-02-09 13:38:02 +0330

    added OpenKeymapSettingsAction

---
+++ src/main/java/ir/mmd/intellijDev/Actionable/extra/ide/keymap/OpenKeymapSettingsAction.kt
@@ -0,0 +1,16 @@
+package ir.mmd.intellijDev.Actionable.extra.ide.keymap
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.keymap.impl.ui.KeymapPanel
+import com.intellij.openapi.options.ShowSettingsUtil
+
+class OpenKeymapSettingsAction : AnAction() {
+	override fun actionPerformed(e: AnActionEvent) {
+		ShowSettingsUtil.getInstance().showSettingsDialog(e.project, KeymapPanel::class.java)
+	}
+	
+	override fun getActionUpdateThread() = ActionUpdateThread.BGT
+	override fun isDumbAware() = true
+}
