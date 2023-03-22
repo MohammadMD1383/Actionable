50d5e8dd6c87e6d68e23b404436a1db8fe618c53
Mohammad Mostafa <ezmmd83@gmail.com>
2023-03-22 11:32:53 +0330

    updated to v3.17.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ fun DependencyHandlerScope.classpath(path: String) {
 }
 
 group = "ir.mmd.intellijDev"
-version = "3.16.0"
+version = "3.17.0"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_1_8
