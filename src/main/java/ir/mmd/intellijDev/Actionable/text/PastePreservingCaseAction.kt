3201741a3ec26cc91c3fd73477c937537574756f
Mohammad Mostafa <ezmmd83@gmail.com>
2023-03-26 16:28:45 +0330

    closes #37

---
+++ src/main/java/ir/mmd/intellijDev/Actionable/text/PastePreservingCaseAction.kt
@@ -0,0 +1,36 @@
+package ir.mmd.intellijDev.Actionable.text
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import ir.mmd.intellijDev.Actionable.action.LazyEventContext
+import ir.mmd.intellijDev.Actionable.util.ext.*
+import java.awt.Toolkit
+import java.awt.datatransfer.DataFlavor
+
+class PastePreservingCaseAction : AnAction() {
+	override fun actionPerformed(e: AnActionEvent): Unit = (LazyEventContext(e)) {
+		val contents = (Toolkit.getDefaultToolkit().systemClipboard.getData(DataFlavor.stringFlavor) as String).split('\n')
+		val contentsSize = contents.size
+		
+		if (contentsSize == 0) {
+			return
+		}
+		
+		project.runWriteCommandAction {
+			if (caretCount > contentsSize) {
+				contents.forEachIndexed { i, content ->
+					allCarets[i].replaceSelectedText { content toCaseStyleOf it }
+				}
+			} else {
+				allCarets.withEachIndexed { i ->
+					replaceSelectedText { contents[i] toCaseStyleOf it }
+				}
+			}
+		}
+	}
+	
+	override fun getActionUpdateThread() = ActionUpdateThread.BGT
+	override fun isDumbAware() = true
+	override fun update(e: AnActionEvent) = e.enableIf { hasProject and hasEditor and allCarets.haveSelection }
+}
