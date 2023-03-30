342379a9da81146d242588e1b4662b74dc630d1d
Mohammad Mostafa <ezmmd83@gmail.com>
2023-03-30 14:43:29 +0330

    fixed a bug in JustifyAction

--- src/main/java/ir/mmd/intellijDev/Actionable/caret/justification/JustifyAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/caret/justification/JustifyAction.kt
@@ -3,16 +3,13 @@ package ir.mmd.intellijDev.Actionable.caret.justification
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import ir.mmd.intellijDev.Actionable.util.ext.editor
-import ir.mmd.intellijDev.Actionable.util.ext.enableIf
-import ir.mmd.intellijDev.Actionable.util.ext.haveSelection
-import ir.mmd.intellijDev.Actionable.util.ext.justifier
+import ir.mmd.intellijDev.Actionable.util.ext.*
 
 abstract class JustifyAction(private val justify: JustifyCaretUtil.() -> Unit) : AnAction() {
 	override fun getActionUpdateThread() = ActionUpdateThread.BGT
 	override fun isDumbAware() = true
 	override fun actionPerformed(e: AnActionEvent) = e.editor!!.justifier.justify()
 	override fun update(e: AnActionEvent) = e.enableIf {
-		hasProject && hasEditor && caretCount > 1 && allCarets.haveSelection
+		hasProject && hasEditor && caretCount > 1 && allCarets.dontHaveSelection
 	}
 }
