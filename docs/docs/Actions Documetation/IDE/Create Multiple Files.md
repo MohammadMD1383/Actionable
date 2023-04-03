14d5c988d4eb24b1c279d3a7b943f1f3f1eba005
Mohammad Mostafa <ezmmd83@gmail.com>
2023-04-03 15:11:04 +0330

    closes #72

--- docs/docs/Actions Documetation/IDE/Create Multiple Files.md
+++ docs/docs/Actions Documetation/IDE/Create Multiple Files.md
@@ -1,1 +1,11 @@
 Creates multiple files in a directory at once from your input. _(separated by `\n`)_
+
+[//]: # (@formatter:off)
+!!! note
+    You can also create files withing directories like this:
+
+    ```
+    path/to/file.txt
+    ```
+
+[//]: # (@formatter:on)
--- src/main/java/ir/mmd/intellijDev/Actionable/ide/CreateMultipleFilesAction.kt
+++ src/main/java/ir/mmd/intellijDev/Actionable/ide/CreateMultipleFilesAction.kt
@@ -1,11 +1,13 @@
 package ir.mmd.intellijDev.Actionable.ide
 
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.vfs.findOrCreateDirectory
 import com.intellij.util.application
 import ir.mmd.intellijDev.Actionable.action.ActionBase
 import ir.mmd.intellijDev.Actionable.action.LazyEventContext
 import ir.mmd.intellijDev.Actionable.ui.showMultilineInputDialog
 import ir.mmd.intellijDev.Actionable.util.ext.getParentDirectory
+import kotlin.io.path.Path
 
 class CreateMultipleFilesAction : ActionBase(), DumbAware {
 	context(LazyEventContext)
@@ -16,7 +18,13 @@ class CreateMultipleFilesAction : ActionBase(), DumbAware {
 		
 		application.runWriteAction {
 			files.forEach {
-				directory.createChildData(this, it)
+				if ('/' in it) {
+					val path = Path(it)
+					directory.findOrCreateDirectory(path.parent.toString())
+						.findOrCreateChildData(this, path.fileName.toString())
+				} else {
+					directory.findOrCreateChildData(this, it)
+				}
 			}
 		}
 	}
