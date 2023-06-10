32b9d12a8a7fa81cc1602e1beba4b7c9d823e921
Mohammad Mostafa <ezmmd83@gmail.com>
2023-06-10 16:07:28 +0330

    added quote handler

---
+++ src/main/java/ir/mmd/intellijDev/Actionable/find/advanced/lang/AdvancedSearchQuoteHandler.kt
@@ -0,0 +1,7 @@
+package ir.mmd.intellijDev.Actionable.find.advanced.lang
+
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler
+import ir.mmd.intellijDev.Actionable.find.advanced.lang.psi.AdvancedSearchTypes.DOUBLE_QUOTE
+import ir.mmd.intellijDev.Actionable.find.advanced.lang.psi.AdvancedSearchTypes.SINGLE_QUOTE
+
+class AdvancedSearchQuoteHandler : SimpleTokenSetQuoteHandler(SINGLE_QUOTE, DOUBLE_QUOTE)
